5cbcea84c8092a00a306c0d56607f65bae618b69
Nihon <mjeczmien2007@gmail.com>
2022-04-28 22:00:44 +0200

    first commit

---
+++ Models/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace MvcCmsRider.Models;
+
+public class Post
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Ccontent { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Published { get; set; }
+    public int AuthorId { get; set; }
+    
+}
